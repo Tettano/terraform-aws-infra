4cb67ee22cae77adcc2b4d281e86bc4e46803fc7
Tettano <shawnmarkr@gmail.com>
2026-03-05 23:15:34 +0800

    revert backend to use dynamodb_tb

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "terraform-state-tettano" # your S3 bucket name
     key            = "terraform.tfstate"
     region         = "ap-southeast-1"
-    dynamodb_table = true
+    dynamodb_table = "terraform-state-lock"
     encrypt        = true
   }
 }
